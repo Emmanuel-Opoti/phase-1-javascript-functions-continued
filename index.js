28f44e49dc36c072b54a7674d89449560e681179
Emmanuel Opoti <opoti63@gmail.com>
2022-11-24 07:52:20 +0300

    complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visFlair){
+    return function (adjective = 'special'){
+        return `You are ${visFlair}${adjective}${visFlair}!`
+    }
+}
+
+console.log(wrapAdjective('!')('nice'))
